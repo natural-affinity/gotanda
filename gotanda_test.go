661449ce452f70dacb942c66a44236a45cca279f
Rizwan Tejpar <natural-affinity@users.noreply.github.com>
2019-01-20 23:39:24 -0800

    add tests for remaining functions

--- gotanda_test.go
+++ gotanda_test.go
@@ -2,6 +2,7 @@ package gotanda_test
 
 import (
 	"errors"
+	"fmt"
 	"testing"
 
 	"github.com/natural-affinity/gotanda"
@@ -26,6 +27,59 @@ func TestCompareError(t *testing.T) {
 		if actual != tc.Expected {
 			t.Errorf("Test: %s\nActual: %t\nExpected: %t\n",
 				tc.Name, actual, tc.Expected)
+		}
+	}
+}
+
+func TestRun(t *testing.T) {
+	cases := []struct {
+		Name    string
+		Command string
+		Output  string
+		Error   error
+	}{
+		{"stdout", "echo hello", "hello\n", nil},
+		{"stderr", "fake-exe", "sh: fake-exe: command not found\n", errors.New("exit status 127")},
+	}
+
+	for _, tc := range cases {
+		obyte, e := gotanda.Run(tc.Command)
+		ostr := string(obyte)
+
+		if ostr != tc.Output {
+			t.Errorf("Test: %s\nActual: %s\nExpected: %s\n",
+				tc.Name, ostr, tc.Output)
+		}
+
+		if !gotanda.CompareError(e, tc.Error) {
+			t.Errorf("Test: %s\nActual: %s\nExpected: %s\n",
+				tc.Name, e, tc.Error)
+		}
+	}
+}
+
+func TestLoadTestFile(t *testing.T) {
+	cases := []struct {
+		Name     string
+		Expected string
+	}{
+		{"load.file.test", "example load file\n"},
+	}
+
+	for _, tc := range cases {
+		dir := "testdata"
+		fp, data := gotanda.LoadTestFile(t, dir, tc.Name+".input")
+
+		expectedPath := fmt.Sprintf("%s\\%s.input", dir, tc.Name)
+		if fp != expectedPath {
+			t.Errorf("Test: %s\nActual: %s\nExpected: %s\n",
+				tc.Name, fp, expectedPath)
+		}
+
+		datastr := string(data)
+		if datastr != tc.Expected {
+			t.Errorf("Test: %s\nActual: %s\nExpected: %s\n",
+				tc.Name, datastr, tc.Expected)
 		}
 	}
 }
